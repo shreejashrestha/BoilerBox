78c176df6536b34c46fa300f020d74ccc6f884ac
shreejashrestha <shresth1@purdue.edu>
2020-09-25 15:08:58 -0400

    button press to next page completed

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'homePage.dart';
+import 'modesPage.dart';
 
 final List<String> entries = <String>[
   'Connect',
@@ -27,56 +29,62 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
             itemCount: 4,
             itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.all(16),
-                child: Stack(
-                  children: <Widget>[
-                    Card(
-                      elevation: 12,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      color: Colors.white,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24.0, vertical: 36),
-                        decoration: BoxDecoration(
+              return new GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ModesPage()));
+                },
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  child: Stack(
+                    children: <Widget>[
+                      Card(
+                        elevation: 12,
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(24),
-                          gradient: LinearGradient(
-                              colors: [Color(0xFF000000), Color(0xFF000000)]),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            FlutterLogo(size: 48),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    '${entries[index]}',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline5
-                                        .copyWith(color: Colors.white),
-                                  ),
-                                ],
+                        color: Colors.white,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 36),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(
+                                colors: [Color(0xFF000000), Color(0xFF000000)]),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              FlutterLogo(size: 48),
+                              SizedBox(
+                                width: 16,
                               ),
-                            ),
-                            Icon(
-                              Icons.navigate_next,
-                              size: 36,
-                              color: Colors.white,
-                            )
-                          ],
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      '${entries[index]}',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline5
+                                          .copyWith(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Icon(
+                                Icons.navigate_next,
+                                size: 36,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             }),
