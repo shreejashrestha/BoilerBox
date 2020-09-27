e7e5c1a29c4941ca2c2697e918151bf7e94aea9e
shreejashrestha <shresth1@purdue.edu>
2020-09-26 20:23:22 -0400

    Bluetooth page added, with functionality

--- lib/connectPage.dart
+++ lib/connectPage.dart
@@ -3,6 +3,7 @@
 ///
 
 import 'package:flutter/material.dart';
+import 'BluetoothFolder/btmain.dart';
 
 final List<String> entries = <String>['Spotify', 'Bluetooth'];
 
@@ -26,57 +27,62 @@ class _ConnectPageState extends State<ConnectPage> {
         child: ListView.builder(
             itemCount: 2,
             itemBuilder: (context, index) {
-              //Navigator.pop(context)
-              return Container(
-                margin: EdgeInsets.all(16),
-                child: Stack(
-                  children: <Widget>[
-                    Card(
-                      elevation: 12,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      color: Colors.white,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24.0, vertical: 36),
-                        decoration: BoxDecoration(
+              return new GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MainPage()));
+                },
+                child: Container(
+                  margin: EdgeInsets.all(16),
+                  child: Stack(
+                    children: <Widget>[
+                      Card(
+                        elevation: 12,
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(24),
-                          gradient: LinearGradient(
-                              colors: [Color(0xFF000000), Color(0xFF000000)]),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            FlutterLogo(size: 48),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    '${entries[index]}',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline5
-                                        .copyWith(color: Colors.white),
-                                  ),
-                                ],
+                        color: Colors.white,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 36),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24),
+                            gradient: LinearGradient(
+                                colors: [Color(0xFF000000), Color(0xFF000000)]),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              FlutterLogo(size: 48),
+                              SizedBox(
+                                width: 16,
                               ),
-                            ),
-                            Icon(
-                              Icons.navigate_next,
-                              size: 36,
-                              color: Colors.white,
-                            )
-                          ],
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(
+                                      '${entries[index]}',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline5
+                                          .copyWith(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Icon(
+                                Icons.navigate_next,
+                                size: 36,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             }),
