510c0d0a5889852a2d690177fa96d877b24709e1
shreejashrestha <shresth1@purdue.edu>
2020-10-20 13:25:47 -0400

    spotify page updated for http requests, and converting json to dart

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# ui
+# BoilerBox
 
-A new Flutter application.
+A new app designed and developed by team 24. 
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This app:
+1. provides connection to the BoilerBox through Bluetooth connection
+2. authorizes access with Spotify and retrieves song data
+3. allows user to change preferences for LEDs 
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/SpotifyFolder/spotifymain.dart
+++ lib/SpotifyFolder/spotifymain.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+// import 'package:spotify/spotify.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
-final List<String> entries = <String>[
-  'Connect',
-  'Modes',
-  'Settings',
-  'Profile'
-];
+//class of track. holds returned info
+class Track {
+  // final int id;
+  // final String uri;
+  final String punchline;
+
+  Track(this.punchline);
+
+  Track.fromJson(Map<String, dynamic> json)
+      // : id = json['id'],
+      // uri = json['uri'],
+      : punchline = json['punchline'];
+
+  Map<String, dynamic> toJson() => {
+        // 'id': id,
+        'punchline': punchline,
+        // 'uri': uri,
+      };
+}
+
+// http get. returns converted json into dart class
+Future<Track> fetchTrack() async {
+  final response =
+      await http.get('https://official-joke-api.appspot.com/random_joke');
+
+  Map trackMap = jsonDecode(response.body);
+  var track = Track.fromJson(trackMap);
+
+  return track;
+}
 
 class SpotifyPage extends StatefulWidget {
   SpotifyPage({Key key, this.title}) : super(key: key);
@@ -14,14 +42,43 @@ class SpotifyPage extends StatefulWidget {
   _SpotifyPage createState() => _SpotifyPage();
 }
 
+// page
 class _SpotifyPage extends State<SpotifyPage> {
+  Future<Track> futureTrack;
+
+  @override
+  void initState() {
+    super.initState();
+    futureTrack = fetchTrack();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        title: Text('Spotify'),
+    return MaterialApp(
+      title: 'Fetch Data',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          title: Text('Fetch Data'),
+        ),
+        body: Center(
+          child: FutureBuilder<Track>(
+            future: futureTrack,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.punchline);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
